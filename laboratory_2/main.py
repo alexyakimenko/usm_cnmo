1100d79e365f5ff8d3341e930777e12d01cf32ce
Alex <alexyakimenkojr@gmail.com>
2025-03-12 00:01:09 +0200

    fix spelling

--- laboratory_2/main.py
+++ laboratory_2/main.py
@@ -5,7 +5,7 @@ EPSILON = 10e-4
 
 class Method(Enum):
     Gauss = 0
-    Yakob = 1
+    Jacob = 1
     
 def system_solve(method: Method) -> Tuple[Tuple[float, ...], int]:
     x1 = x2 = x3 = x4 = 0
@@ -14,7 +14,7 @@ def system_solve(method: Method) -> Tuple[Tuple[float, ...], int]:
     while True:
         old_x1, old_x2, old_x3, old_x4 = x1, x2, x3, x4
 
-        if method == Method.Yakob:
+        if method == Method.Jacob:
             x1 = 0.605 / 2.923 - 0.220 * old_x2 / 2.923 - 0.159 * old_x3 / 2.923 - 0.328 * old_x4 / 2.923
             x2 = 0.496 / 4.123 - 0.363 * old_x1 / 4.123 - 0.268 * old_x3 / 4.123 - 0.327 * old_x4 / 4.123
             x3 = 0.590 / 3.906 - 0.169 * old_x1 / 3.906 - 0.271 * old_x2 / 3.906 - 0.295 * old_x4 / 3.906
@@ -30,7 +30,7 @@ def system_solve(method: Method) -> Tuple[Tuple[float, ...], int]:
             return ((x1, x2, x3, x4), iterations)
         
 
-(result1, iterations1) = system_solve(method=Method.Yakob)
+(result1, iterations1) = system_solve(method=Method.Jacob)
 print(result1, iterations1)
 (result2, iterations2) = system_solve(method=Method.Gauss)
 print(result2, iterations2)
